9be83b2cd60857fcf6f08cc8b5629b4a4229fd83
Matt Johnson <johnso87@illinois.edu>
2014-02-15 15:46:14 -0600

    Trim fat, use 6th order finite difference formula for derivative, remove b.s. from r calculation.
    TODO Need an intuitive way to specify r/h values for connections b/w objects; as it is, it's guess and check.

--- sim/sim.py
+++ sim/sim.py
@@ -65,11 +65,11 @@ class PIDSimEventPerturbMass(PIDSimEventBase):
 	order_key = 2
 
 class PIDSimEventAddConnection(PIDSimEventBase):
-	def __init__(self, obj1, obj2, seconds_to_99_percent_transfer):
+	def __init__(self, obj1, obj2, r):
 		self.obj1 = obj1
 		self.obj2 = obj2
-		#If e^(-r*t) = .99, r=.01005/t
-		self.r = .01005/seconds_to_99_percent_transfer
+		self.r = r
+		print 'connection b/w %s & %s, r=%f' % (obj1.name, obj2.name, self.r)
 	def apply(self, eventq, current_timestep, all_objects, connections):
 		if self.obj1 in all_objects and self.obj2 in all_objects:
 			connections[self.obj1, self.obj2] = self.r
@@ -104,10 +104,6 @@ def print_all_objects(ts, objects):
 	for o in objects:
 		print '%s, temp %f C, mass %fg, specific heat %fg' % (o.name, o.temp, o.mass, o.specific_heat)
 
-def add_connection(connections, obj1, obj2, seconds_to_99_percent_transfer):
-	#If e^(-r*t) = .99, r=.01005/t
-	connections[obj1, obj2] = .01005/seconds_to_99_percent_transfer
-
 def sim(timestep_size, num_timesteps, events):
 	eventq = [] #Managed with heappush() and heappop() from heapq
 	objects = []
@@ -130,7 +126,8 @@ def sim(timestep_size, num_timesteps, events):
 				hotter, colder = pair[0], pair[1]
 			else:
 				hotter, colder = pair[1], pair[0]
-			h = connections[pair] #Value in dict if heat transfer coefficient
+			#This seems like a plausible heat transfer model but I don't yet know how to calculate plausible h values.
+			h = connections[pair] #Value in dict is heat transfer coefficient
 			dQdt = h * (hotter.temp - colder.temp)
 			dQ = dQdt * timestep_size
 			hotter.temp = hotter.temp - (dQ / (hotter.mass*hotter.specific_heat))
@@ -138,7 +135,7 @@ def sim(timestep_size, num_timesteps, events):
 		#PID control comes last
 		water = find(lambda obj: obj.name == 'Water', objects)
 		output = controller.control(65, water.temp, (t%100 == 0))
-		heater_wattage = 300
+		heater_wattage = 900
 		PIDSimEventPerturbEnergy(water, heater_wattage*timestep_size*output).apply(events, t, objects, connections)
 		temps.setdefault('pid', []).append((t, output))
 
@@ -161,7 +158,7 @@ def round_partial(value, resolution):
     return round(value / resolution) * resolution
 
 class PIDController:
-	qlen = 5
+	qlen = 7
 	#x_params are each 3-tuples of (scale factor, min val, max val)
 	def __init__(self, p_params, i_params, d_params, min, max):
 		#Circular buffer of last N error values (for filtering and derivatives)
@@ -199,7 +196,12 @@ class PIDController:
 		
 		self.i = clamp(self.i + error, self.i_min, self.i_max)
 		
-		d = clamp(self.d_scale * ((3*self.errorq[0] - 4*self.errorq[1] + 1*self.errorq[2]) / 2.0), self.d_min, self.d_max)
+		d = clamp(self.d_scale * 
+			      ((-147*self.errorq[0] + 360*self.errorq[1] - 450*self.errorq[2]
+			       + 400*self.errorq[3] - 225*self.errorq[4] + 72*self.errorq[5] - 10*self.errorq[6])
+			      / 60.0),
+			      self.d_min,
+			      self.d_max)
 
 		output = clamp(p + self.i + d, self.min, self.max)
 		self.outputq.appendleft(output)
@@ -211,26 +213,26 @@ class PIDController:
 def main(argv=None):
 	if argv is None:
 		argv = sys.argv
-	pottemp = 30
+	pottemp = 45
 	#12 quarts of water
 	water = PIDSimObject('Water', 10886.2, 4.186, pottemp)
 	#2 pounds of pork loin (see http://www.engineeringtoolbox.com/specific-heat-capacity-food-d_295.html)
 	pork = PIDSimObject('Pork', 907.185, 2.76, 5)
 	#2kg of stainless steel
 	pot = PIDSimObject('Pot', 2000.0, 0.51, pottemp)
-	#Environment around pot, assume 65f and infinite mass (will never change in temperature)
-	air = PIDSimObject('Air', float('inf'), 1.006, 18.33)
+	#Environment around pot, assume 65f and very large mass (will never change in temperature)
+	air = PIDSimObject('Air', 1.0e10, 1.006, 18.33)
 	events = []
 	events.append(PIDSimEventInstance(PIDSimEventPutIn(water), 0))
 	events.append(PIDSimEventInstance(PIDSimEventPutIn(pork), 0))
 	events.append(PIDSimEventInstance(PIDSimEventPutIn(pot), 0))
 	events.append(PIDSimEventInstance(PIDSimEventPutIn(air), 0))
-	events.append(PIDSimEventInstance(PIDSimEventAddConnection(water, pork, .01), 0))
-	events.append(PIDSimEventInstance(PIDSimEventAddConnection(water, pot, .01), 0))
-	events.append(PIDSimEventInstance(PIDSimEventAddConnection(pot, air, .25), 0))
-	events.append(PIDSimEventInstance(PIDSimEventAddConnection(water, air, .08), 0))
+	events.append(PIDSimEventInstance(PIDSimEventAddConnection(water, pork, 2), 0))
+	events.append(PIDSimEventInstance(PIDSimEventAddConnection(water, pot, 5), 0))
+	events.append(PIDSimEventInstance(PIDSimEventAddConnection(pot, air, .5), 0))
+	events.append(PIDSimEventInstance(PIDSimEventAddConnection(water, air, .2), 0))
 
-	results = sim(1, 5000, events)
+	results = sim(1, 10000, events)
 	for o, l in results.iteritems():
 		times, temps = zip(*l)
 		#Keys are either strings or PIDSimObjects
